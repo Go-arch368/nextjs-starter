274bc5c93c9f1eb4752a2a43e7929f229b24d815
Gowtham T <gowthamtj0808@gmail.com>
2025-04-12 21:11:04 +0530

    CHANGES

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -176,9 +176,7 @@ export default function Navbar() {
 
             <SignedOut>
               <SignInButton>
-                <button className="hidden rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-white hover:bg-green-800 md:block">
-                 
-                </button>
+                <button className="hidden rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-white hover:bg-green-800 md:block"></button>
               </SignInButton>
             </SignedOut>
             <SignedIn>
